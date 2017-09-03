3cfee8bc76055486cc670c8b682893312e38b63a
Peter Scicluna <peterscicluna@asiaa.sinica.edu.tw>
2017-09-04 07:14:54 +0800

    some initial things

---
+++ phiresstarter/__init__.py
@@ -0,0 +1,25 @@
+"""
+Welcome to PhIReSSTARTer
+
+"""
+
+__version__ = "0.0.0"
+__copyright__ = """ Copyright (C) 2017  P. Scicluna, F. Kemper, S. Srinivasan
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+""" #Add a statement like this to each file/module/subpackage we include. Exactly how it should be included is a matter of debate... but including it in variables makes it both part of the source and part of the program itself, meaning it is accessible in python.
+
+__license__ = "GNU Public License v3" #
+
+all=[] #Nothing to import just yet
