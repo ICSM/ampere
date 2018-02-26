2241feddb6c06f365a80ffdefea07b71830884c7
lfanciullo <lfanciullo@asiaa.sinica.edu.tw>
2018-02-27 00:10:35 +0800

    Started __init__ section (read opacity files and construct matrix). Not finished

--- phiresstarter/ClassRT.py
+++ phiresstarter/ClassRT.py
@@ -14,7 +14,14 @@ class RadTransWrap(object):
     Output: model fluxes (modelFlux)'''
 
 	def __init__(self, **kwargs):
-            pass
+            import os#, os.path
+            opacityDirectory = './Opacities/'
+            opacityFileList = os.listdir(opacityDirectory)
+            opacityFileList.sort()
+            nSpecies = opacityFileList.__len__() # Warning: need to check all files found are ordinary files (not directory or such). Look up os.path.isfile()
+            opacityData = np.loadtxt(opacityDirectory+'ss_Dorschneretal1995_Olivine_0.10.q', comments='#')
+            # To finish: Construct opacity_matrix
+           pass
         
         def __call__(self, multiplicationFactor = 1, powerLawIndex = 2, relativeAbundances = np.ones(nSpecies)/nSpecies, **kwargs):
             fModel = (np.matmul(opacity_matrix, relativeAbundances)+1)*wavelengths**powerLawIndex*multiplicationFactor
