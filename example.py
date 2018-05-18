354ad60b8658baae629689154e599070df8d5639
Jonty <jmarshall@asiaa.sinica.edu.tw>
2018-05-18 15:23:57 +0800

    Simple BB model
    Modelling!

--- example.py
+++ example.py
@@ -1,12 +1,13 @@
 import numpy as np
 from ampere.data import Spectrum, Photometry
 from ampere.emceesearch import EmceeSearch
-from ampere.PowerLawAGN import PowerLawAGN
+from ampere.PowerLawAGN import PowerLawAGN, SingleModifiedBlackBody
 from ampere.extinction import CCMExtinctionLaw
 import corner
 import matplotlib.pyplot as plt
 
-
+from astropy import constants as const
+from astropy import units as u
 if __name__=="__main__":
     """
     This script is a demonstration of how ampere (should) work(s). 
@@ -14,11 +15,20 @@ if __name__=="__main__":
 
 
     """ Read in some data """
-    irs = Spectrum.fromArray(filename)
+    filename = 'INSERT_AMPERE_DIR_HERE/Testdata/cassis_yaaar_spcfw_14203136t.fits'
+    irs = Spectrum.fromFile(filename,format='SPITZER-YAAAR')
     #If we have more than one dataset we need multiple data objects
-
+    
     """ Define the model """
-    model = PowerLawAGN(irs.wavelengths) #We should probably define a wavelength grid separate from the data wavelengths for a number of reasons, primarily because the photometry needs an oversampled spectrum to compute synthetic photometry
+    Tbb = 150.0
+    Area = (10.*u.au.value)^2
+    TestData = SingleModifiedBlackBody(irs.wavelength,flatprior=True,
+                 normWave = 20., sigmaNormWave = 100.,redshift = False)
+    TestData(t = Tbb, scale = Area, index = 0., dist=3.)
+    irs['flux'].data = TestData.modelFlux
+    irs['uncertainty'].data = 0.11*irs['flux'].data
+    
+    #model = PowerLawAGN(irs.wavelengths) #We should probably define a wavelength grid separate from the data wavelengths for a number of reasons, primarily because the photometry needs an oversampled spectrum to compute synthetic photometry
     
     """ and the extinction law (if necessary) (could be moved inside model as only some models will need this) """
     ext = CCMExtinctionLaw()
