feafc67f308ea42a24c1e006c59d1ea51545bf54
Thavisha <tdharmawardena@asiaa.sinica.edu.tw>
2018-10-01 11:33:12 +0800

    creating a model set of data

--- examples/modbbtest.py
+++ examples/modbbtest.py
@@ -21,15 +21,26 @@ if __name__=="__main__":
     wavelengths = 10**np.linspace(0.,1.9, 2000)
 
     """ Choose some model parameters  """
+    t_in = 200. #units = K (or 300K)
+    scale_in = 1.
+    index_in = 0.
+    distance = 1.
+    lims_in=np.array([ [100., 300.], [0., 2.], [-5, 5], [0.9, 1.1]
+                  ])
 
 
     """ Initialise the model  """
+    model = SingleModifiedBlackBody(wavelengths, lims=lims_in)
 
 
     """ Get a test spectrum out of the model """
+    #model(t_in, scale_in, index_in, distance)
+    #model_flux = model.modelFlux
+    model_flux = model(t_in, scale_in, index_in, distance).modelFlux #Do the two lines above with just one line here. 
 
 
     """ get synthetic photometry and spectra """
+    
 
 
     """ (optionally) add some noise """
