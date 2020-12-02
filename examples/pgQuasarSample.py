4fdd849f29b6ded5cbdf648d5af6e7d4bde086e3
Ciska Kemper <ciska@asiaa.sinica.edu.tw>
2020-12-02 16:53:22 +0100

    Trying to apply selectWaves on the irs data in pgQuasarSample. Not successful yet.

--- examples/pgQuasarSample.py
+++ examples/pgQuasarSample.py
@@ -21,25 +21,29 @@ if __name__=="__main__":
     specFile = 'cassis_yaaar_spcfw_14191360t.fits'
     photFile = 'vizier_votable_pruned_no2MASS.vot'
     irs = Spectrum.fromFile(dataDir+specFile,format='SPITZER-YAAAR')
+    #irs = [chunk.selectWaves(low = 8., up=35.) for chunk in irs] # alternative to next two lines, doesn't work
+    irs[0] = irs[0].selectWaves(low = 8., up = 35.) #following Srinivasan et al. 2017
+    irs[1] = irs[1].selectWaves(low = 8., up = 35.) #following Srinivasan et al. 2017
+    print("hello")
+    print(irs)
+    # irs is now a list of two empty things. This does not work.
     libDir = '../ampere/'
     libname = libDir + 'ampere_allfilters.hd5'
     phot = Photometry.fromFile(dataDir+photFile, libName = libname)
+    phot = phot.selectWaves(low = 35., interval = "right-open") #using only MIPS-70 and PACS, following Srinivasan et al. 2017
     print(phot)
     modwaves = 10**np.linspace(0.,1.9, 2000)
 
     model = PowerLawAGN(modwaves, redshift=0.058)
     phot.reloadFilters(modwaves)
-    dataSet = [phot]          #comment in when using photometry
+    dataSet = [phot]          #use this line when using photometry
     #dataSet = [s for s in irs] #comment out when using photometry
-    print(phot)
-    print(irs)
-    for s in irs:             #comment the next four lines in when appending spectroscopy to photometry
+    print(type(phot))
+    print(type(irs))
+    for s in irs:             #include the next four lines when appending spectroscopy to photometry
         dataSet.append(s)
     for s in dataSet:
         print(s)
-
-    # Srinivasan et al. (2017) have performed the fit over the observed 8-35 micron range, combined with the longer wavelength PACS or MIPS-70 photometry, if any.
-    # We thus need to constrain dataSet accordingly to compare
 
     opt = EmceeSearch(model = model, data = dataSet, nwalkers = 200)
 
