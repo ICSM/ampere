dd24e66e35f5205258ad05b9c455e3f44b02cabb
jontymarshall <32949295+jontymarshall@users.noreply.github.com>
2021-12-09 17:52:43 +0800

    Hyperion class initial revision
    Starting to put the Hyperion model class into the same structure as the other model types in Ampere - broken up the __init__ call to separate functions that add a source, dust component(s), and specify the model properties. Need to allow it to deal with multiple components iteratively (in a for loop in the script that calls this model class, or internally?).

--- ampere/Hyperion.py
+++ ampere/Hyperion.py
@@ -25,30 +25,54 @@ class HyperionRTModel(Model):
 
     '''
 
-    def __init__(self, par, **kwargs):
+    def __init__(self,flatprior=True,opacityFileList=None,parameters=par,**kwargs):
 
+        self.components = components #number of sub-models to run to generate complete model
+        self.distribution = distribution #dust density distribution + its properties, somehow?
+        #maybe have standard models in here - power law shell = 3 parameters (S0, Rin, alpha_out), 
+        #                                     power law disc = 5 parameters (S0, Rin, Rout, h, alpha_out),
+        #                                     2 power law disc = 5 parameters (S0, R0, h, alpha_in, alpha_out), 
+        #                                     Gaussian annulus = 4 parameters (S0, R0, dR, h)
+        self.npars += distribution.nParameters
+
+        self.flatprior = flatprior
+        #Define opacities for use in model calculation - probably only going to be a single species in most cases, but
+        #should make it general for multiple species per component, and multiple components (see above)
+        import os
+        from scipy import interpolate
+        opacityDirectory = os.path.dirname(__file__)+'/Opacities/'
+        opacityFileList = np.array(opacityFileList)
+        #opacityFileList = np.array(opacityFileList)[['.q' in zio for zio in opacityFileList]] # Only files ending in .q are valid (for now)
+        nSpecies = opacityFileList.__len__()
+        #print(opacityFileList,nSpecies)
+        opacity_array = np.zeros((wavelengths.__len__(), nSpecies))
+        for j in range(nSpecies):
+            tempData = np.loadtxt(opacityDirectory + opacityFileList[j], comments = '#')
+            tempWl = tempData[:, 0]
+            tempOpac = tempData[:, 1]
+            f = interpolate.interp1d(tempWl, tempOpac, assume_sorted = False)
+            opacity_array[:,j] = f(self.wavelength)
+        self.opacity_array = opacity_array
+        self.nSpecies = nSpecies
+        self.npars += nSpecies - 1 + 4       
+    
+        model = Model()
+
+        return model
+
+    def HyperionRTAddDust(self,par,**kwargs):
         #Set up dust model for Hyperion
         # Initalize the model
-        m = Model()
         
-        #Set central source position
-        m.add_spherical_source(luminosity  = par.get('lsource',1.0*lsun),
-                               radius      = par.get('rsource',1.0*rsun),
-                               mass        = par.get('msource',1.0*msun),
-                               temperature = par.get('tsource',5784.0),
-                               position    = par.get('position',(0.0,0.0,0.0))
-        
-        # Use raytracing to improve s/n of thermal/source emission
-        m.set_raytracing(par.get('raytracing',True))
-        
-        # Use the modified random walk
-        m.set_mrw(par.get('modrndwlk',True), gamma=par.get('mrw_gamma',2))
-
-        # Set up spatial grid
+        from astropy.units import au
+        #Add the grid to the model
+        #Set up spatial grid
+        #The grid limits here should be dynamic - set up to fit the largest extent of observations or a 
+        #specific field of view or some such
         x = np.linspace(-1.5*par.get('rmax',100.*u.au), 1.5*par.get('rmax',100.*u.au), par.get('ngrid',257))
         y = np.linspace(-1.5*par.get('rmax',100.*u.au), 1.5*par.get('rmax',100.*u.au), par.get('ngrid',257))
         z = np.linspace(-1.5*par.get('rmax',100.*u.au), 1.5*par.get('rmax',100.*u.au), par.get('ngrid',257))
-        m.set_cartesian_grid(x,y,z)
+        model.set_cartesian_grid(x,y,z)
 
         print("Spatial grid set up.")
 
@@ -60,39 +84,71 @@ class HyperionRTModel(Model):
         rmin = par.get('rmin',70.*u.au)
         rmax = par.get('rmax',100.*u.au)
         rmid = (rmax + rmin) / 2
-        rr = np.sqrt(m.grid.gx ** 2 + m.grid.gy ** 2 + m.grid.gz ** 2)
+        
+        rr = np.sqrt(model.grid.gx ** 2 + model.grid.gy ** 2 + model.grid.gz ** 2)
         
         #define density grid
-        density = np.zeros(m.grid.shape)
-        density = rho0 * ( (rr/rmid)**(2.*alpha_in) + (rr/rmid)**(2.*alpha_out) )**(-0.5) * np.exp(-((abs(m.grid.gz)/rr)**2/scaleheight**2)/2.0)
-        m.add_density_grid(density, d)
+        density = eval(distribution + "(model,par)") #np.zeros(model.grid.shape)
+        
+        model.add_density_grid(density, d)
 
         print("Density grid set up.")
+
+    def HyperionRTAddSource(self,par,**kwargs):
+        #Add the [central] source to the model - should be either spherical or point source
+        #Can also specify spectrum as part of the inputs...    
+        if par.any('source'):
+            #Set central source position
+            model.add_spherical_source(luminosity  = par.get('lsource',1.0*lsun),
+                                        radius      = par.get('rsource',1.0*rsun),
+                                        mass        = par.get('msource',1.0*msun),
+                                        temperature = par.get('tsource',5784.0),
+                                        position    = par.get('position',(0.0,0.0,0.0))
+
+        print("Source set up.")
+
+
+    def HyperionRTSetup(self,par,**kwargs):
+        #This is a catch all for the bits and pieces that actually run the model
+        #The number of photons, ray tracing, wavelengths, all that jazz
+
         
-        # Set up SED for 10 viewing angles
-        sed = m.add_peeled_images(sed=par.get('api_sed',True), image=par.get('api_img',False))
-        sed.set_viewing_angles(np.linspace(0., 90., 10), np.repeat(45., 10))
-        sed.set_wavelength_range(par.get('nl',101), par.get('lmin',0.1), par.get('lmax',1000.))
-        sed.set_track_origin('basic')
+        #Setup wavelengths
         
-        # Set number of photons
-        m.set_n_photons(initial=par.get('nph_initial',1e4), imaging=par.get('nph_imging',1e5),
+
+        #Use raytracing to improve s/n of thermal/source emission
+        model.set_raytracing(par.get('raytracing',True))
+        
+        #Use the modified random walk
+        model.set_mrw(par.get('modrndwlk',True), gamma=par.get('mrw_gamma',2))
+        
+        #Set up SED for 10 viewing angles
+        model.add_peeled_images(sed=par.get('api_sed',True), image=par.get('api_img',False))
+        model.set_viewing_angles(np.linspace(0., 90., 10), np.repeat(45., 10))
+        model.set_wavelength_range(par.get('nl',101), par.get('lmin',0.1), par.get('lmax',1000.))
+        model.set_track_origin('basic')
+        
+        #Set number of photons
+        model.set_n_photons(initial=par.get('nph_initial',1e4), imaging=par.get('nph_imging',1e5),
                         raytracing_sources=par.get('nph_rtsrcs',1e5), raytracing_dust=par.get('nph_rtdust',1e5))
         
-        # Set number of temperature iterations
-        m.set_n_initial_iterations(par.get('niter',5))
-        
-        # Write out file
-        m.write('HyperionRT_sed.rtin')
-        print("Hyperion RT model created.")
+        #Set number of temperature iterations
+        model.set_n_initial_iterations(par.get('niter',5))
+
+        print("Model setup complete.")
 
     def __call__(self, *args, **kwargs):
-        m.run('HyperionRT_sed.rtout', mpi=True,n_processes=6,overwrite=True)
+
+        model.write('HyperionRT_sed.rtin')
+        print("Hyperion RT model created.")
+        model.run('HyperionRT_sed.rtout', mpi=True,n_processes=6,overwrite=True)
         print("Hyperion RT model executed.")
 
-        m = ModelOutput('HyperionRT_sed.rtout')
+        result = ModelOutput('HyperionRT_sed.rtout')
 
-        self.modelFlux = sed.value
+        #Save both the model and the results
+        self.model = model
+        self.result = result
 
     def lnprior(self, theta, **kwargs):
         if self.flatprior:
@@ -159,51 +215,15 @@ class HyperionRTModel(Model):
 
         return par
 
-#convenience function to plot the SED of the Hyperion RT output
-    def plot_sed(par):
-        #Set up figure
-        import matplotlib.pyplot as plt
-        fig = plt.figure()
-        ax = fig.add_subplot(1, 1, 1)
-        
-        #Read in Hyperion model output
-        try:
-            m = ModelOutput('HyperionRT_sed.rtout')
+    def HyperionRTPlot(self):
+        #
+        # Insert code here to call the plotting routines for the respective data classes
+        # and Hyperion model.
+        #
 
-            #Total SED
-            sed = m.get_sed(inclination=0, aperture=-1, distance=par['distance'],
-                                    component='total',units='Jy')        
-            ax.loglog(sed.wav, sed.val, color='black', lw=3, alpha=0.5)
-            
-            # Direct stellar photons
-            sed = m.get_sed(inclination=0, aperture=-1, distance=par['distance'],
-                                   component='source_emit',units='Jy')
-            ax.loglog(sed.wav, sed.val, color='blue')
-            # Scattered stellar photons
-            sed = m.get_sed(inclination=0, aperture=-1, distance=par['distance'],
-                                   component='source_scat',units='Jy')
-            ax.loglog(sed.wav, sed.val, color='teal')
-            # Direct dust photons
-            sed = m.get_sed(inclination=0, aperture=-1, distance=par['distance'],
-                                   component='dust_emit',units='Jy')
-            ax.loglog(sed.wav, sed.val, color='red')        
-            # Scattered dust photons
-            sed = m.get_sed(inclination=0, aperture=-1, distance=par['distance'],
-                                   component='dust_scat',units='Jy')
-            ax.loglog(sed.wav, sed.val, color='orange')
-
-            ax.set_xlabel(r'$\lambda$ [$\mu$m]')
-            ax.set_ylabel(r'Flux [Jy]')
-            ax.set_xlim(par.get('lmin',0.1), par.get('lmax',1000.))
-            ax.set_ylim(1e-6*np.max(sed.value),1.1*np.max(sed.value))
-            fig.savefig('HyperionRT_sed_plot_components.png')
-            plt.close(fig)
-
-        except IOError:
-            print("No Hyperion RT output found, SED not plotted.")
 
 #convenience function to write dust parameter file '<dust>.params' for Hyperion BHDust calculator (separate program)
-    def write_bhmie_file(par):
+    def HyperionRTBHMie(self,par):
         f=open(par.get('dust','astrosilicate')+'.params','w')
         f.write('"'+par['dust']+'_'+str(par['size'])+'"'+'\n')
         f.write(str(par['format'])+'\n')
