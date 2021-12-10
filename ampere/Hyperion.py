fe2b969a9c9564ebc3aad0d97527c2040c900972
jonty <jonty.marshall@gmail.com>
2021-12-10 15:55:52 +0800

    Revised Hyperion model class
    Put everything into __call__, need to implement multiple dust types into the call, multiple dust components.

--- ampere/Hyperion.py
+++ ampere/Hyperion.py
@@ -56,99 +56,163 @@ class HyperionRTModel(Model):
         self.nSpecies = nSpecies
         self.npars += nSpecies - 1 + 4       
     
-        model = Model()
+        self.model = Model()
 
-        return model
+    def __call__(self,dust="astrosilicate",fileformat=2,amin=0.5,amax=1000.,na=101,
+                        nang=91,nanx=11,
+                        nchem=1,gtd=0,
+                        lmin=0.1,lmax=1000.0,nl=101,
+                        tmin=2.7,tmax=1000.0,nt=101,
+                        massfrac=1.0,
+                        rho0=1.5e-19,
+                        optconst="silicate_d03.lnk",
+                        q=3.5,
+                 #Source parameters
+                        sources=[['spherical',1.0,1.0,1.0,5784,[0.0,0.0,0.0]]], #(type,lstar,rstar,mstar,tstar,position[x,y,z],spectrum file)
+                 #Disc parameters
+                        distribution=['example',30,70,-1], #type,rin,rout,alpha
+                        gridtype='cartesian',
+                        rmax= 100.,
+                        ngrid= 251,
+                 #RT parameters
+                        niter=5,
+                        nph_initial=1e4,
+                        nph_imging=1e5,
+                        nph_rtsrcs=1e5,
+                        nph_rtdust=1e5,
+                        track_mode='origin',
+                        raytracing=True,
+                        modrndwlk=True,
+                        mrw_gamma=2,
+                 #Peel photons to get images
+                        api_sed=True,
+                        api_img=False,
+                        view_angles=np.linspace(0., 90., 10),
+                        view_repeats=np.repeat(45., 10),
+                 #Parallel processing
+                        useMPI=True,
+                        nproc=1,
+                        **kwargs):
 
-    def HyperionRTAddDust(self,par,**kwargs):
-        #Set up dust model for Hyperion
-        # Initalize the model
-        
-        from astropy.units import au
+        #Convenience function to write dust parameter file '<dust>.params' for Hyperion BHDust calculator (separate program)
+        f=open(str(dust)+'.params','w')
+        f.write(str(dust)+'_'+str(size)+'\n')
+        f.write(str(int(fileformat))+'\n')
+        f.write(str(amin)+'\n')
+        f.write(str(amax)+'\n')
+        f.write(str(na)+'\n')
+        f.write(str(nang)+'\n')
+        f.write(str(nanx)+'\n')
+        f.write(str(nchem)+'\n')
+        f.write(str(gtd)+'\n')
+        f.write(str(lmin)+' '+str(lmax)+' '+str(nl)+'\n')
+        f.write(str(nproc)+'\n') #parallel processing version of BHMie
+        f.write(''+'\n')
+        f.write(str(massfrac)+'\n')
+        f.write(str(rho0)+'\n')
+        f.write(str(optconst)+'\n')
+        f.write(str(disttype)+'\n')
+        f.write(str(amin)+' '+str(amax)+' '+str(q)+'\n')
+        f.close()
+
+        print("BHMie dust input file created.")
+
+        import subprocess
+        subprocess.run(['bhmie',param_file])
+
+        print("BHMie dust output file created")
+
+        self.d = BHDust(composition+'_'+str(amin))
+        self.d.optical_properties.extrapolate_nu(5e7, 5e16)
+        self.d.set_lte_emissivities(n_temp=nt,temp_min=tmin,temp_max=tmax)
+
+        print("Read in optical constants.")
+
+        import astropy.units as u
         #Add the grid to the model
         #Set up spatial grid
         #The grid limits here should be dynamic - set up to fit the largest extent of observations or a 
         #specific field of view or some such
-        x = np.linspace(-1.5*par.get('rmax',100.*u.au), 1.5*par.get('rmax',100.*u.au), par.get('ngrid',257))
-        y = np.linspace(-1.5*par.get('rmax',100.*u.au), 1.5*par.get('rmax',100.*u.au), par.get('ngrid',257))
-        z = np.linspace(-1.5*par.get('rmax',100.*u.au), 1.5*par.get('rmax',100.*u.au), par.get('ngrid',257))
-        model.set_cartesian_grid(x,y,z)
-
-        print("Spatial grid set up.")
-
+        if gridtype == 'cartesian':
+            self.x = np.linspace(-1.*rmax*u.au, rmax*u.au, ngrid)
+            self.y = np.linspace(-1.*rmax*u.au, rmax*u.au, ngrid)
+            self.z = np.linspace(-1.*rmax*u.au, rmax*u.au, ngrid)
+            self.model.set_cartesian_grid(self.x,self.y,self.z)
         #Set up density grid
-        rho0 = par.get('rho0',1.5e-19)
-        alpha_in = par.get('alpha_in',-5.)
-        alpha_out = par.get('alpha_out',5.)
-        scaleheight= par.get('scaleheight',0.1)
-        rmin = par.get('rmin',70.*u.au)
-        rmax = par.get('rmax',100.*u.au)
-        rmid = (rmax + rmin) / 2
-        
-        rr = np.sqrt(model.grid.gx ** 2 + model.grid.gy ** 2 + model.grid.gz ** 2)
-        
-        #define density grid
-        density = eval(distribution + "(model,par)") #np.zeros(model.grid.shape)
-        
-        model.add_density_grid(density, d)
+            self.rr = np.sqrt(self.model.grid.gx**2 + self.model.grid.gy**2 + self.model.grid.gz**2)
+            print("Spatial grid set up.")
+        if gridtype == 'polar':
+            print("Not implemented.")
+        if gridtype == 'spherical':
+            print("Not implemented")
 
+        #define density grid
+        self.density = eval(self.distribution[0] + "()")
+        self.model.add_density_grid(self.density, self.d)
         print("Density grid set up.")
 
-    def HyperionRTAddSource(self,par,**kwargs):
-        #Add the [central] source to the model - should be either spherical or point source
-        #Can also specify spectrum as part of the inputs...    
-        if par.any('source'):
-            #Set central source position
-            model.add_spherical_source(luminosity  = par.get('lsource',1.0*lsun),
-                                        radius      = par.get('rsource',1.0*rsun),
-                                        mass        = par.get('msource',1.0*msun),
-                                        temperature = par.get('tsource',5784.0),
-                                        position    = par.get('position',(0.0,0.0,0.0))
+        #Set central source position, add source(s) to the grid
+        for source in sources:
+            if source[i,0] == 'spherical':
+                self.model.add_spherical_source(luminosity  = source[i,1] * u.lsun,
+                                                radius      = source[i,2] * u.rsun,
+                                                mass        = source[i,3] * u.msun,
+                                                temperature = source[i,4] * u.K,
+                                                position    = source[i,5])
+            elif source[i,0] == 'point':
+                #The source spectrum files need to be passed from the data class
+                data = np.loadtxt(direc+spectrumfile, dtype=[('wav', float), ('fnu', float)])
+    
+                # Convert to nu, fnu from erg/cm2/A/s
+                freq = c / (np.array(data['wav'].data[1:])*1e-8)
+                angs = np.array(data['wav'].data[1:])
+                flux = np.array(data['fnu'].data[1:])
+                flux = flux*3.33564095E+04*angs**2
+                nu = []
+                fnu = []
+                for i in range(0,len(freq)-1):
+                    if freq[i] != freq[i-1]:
+                        nu.append(freq[i])
+                        fnu.append(flux[i])
+                
+                nu  = np.array(nu)
+                fnu = np.array(fnu)
+    
+                # Set up the source
+                source = self.model.add_point_source(position = source[i,4])
+                source.luminosity = source[i,1] * u.lsun # [ergs/s]
+                source.spectrum = (nu, fnu)
 
-        print("Source set up.")
-
-
-    def HyperionRTSetup(self,par,**kwargs):
-        #This is a catch all for the bits and pieces that actually run the model
-        #The number of photons, ray tracing, wavelengths, all that jazz
-
-        
-        #Setup wavelengths
-        
+        print("Source(s) set up.")
 
         #Use raytracing to improve s/n of thermal/source emission
-        model.set_raytracing(par.get('raytracing',True))
+        self.model.set_raytracing(raytracing)
         
         #Use the modified random walk
-        model.set_mrw(par.get('modrndwlk',True), gamma=par.get('mrw_gamma',2))
+        self.model.set_mrw(modrndwlk, gamma=mrw_gamma)
         
         #Set up SED for 10 viewing angles
-        model.add_peeled_images(sed=par.get('api_sed',True), image=par.get('api_img',False))
-        model.set_viewing_angles(np.linspace(0., 90., 10), np.repeat(45., 10))
-        model.set_wavelength_range(par.get('nl',101), par.get('lmin',0.1), par.get('lmax',1000.))
-        model.set_track_origin('basic')
+        self.model.add_peeled_images(sed=api_sed, image=api_img)
+        self.model.set_viewing_angles(view_angles,view_repeats)
+        self.model.set_wavelength_range(nl, lmin, lmax)
+        self.model.set_track_origin(track_mode)
         
         #Set number of photons
-        model.set_n_photons(initial=par.get('nph_initial',1e4), imaging=par.get('nph_imging',1e5),
-                        raytracing_sources=par.get('nph_rtsrcs',1e5), raytracing_dust=par.get('nph_rtdust',1e5))
+        self.model.set_n_photons(initial=nph_initial, imaging=nph_imging,
+                        raytracing_sources=nph_rtsrcs, raytracing_dust=nph_rtdust)
         
         #Set number of temperature iterations
-        model.set_n_initial_iterations(par.get('niter',5))
+        self.model.set_n_initial_iterations(niter)
 
-        print("Model setup complete.")
+        print("Hyperion RT Model setup complete.")
 
-    def __call__(self, *args, **kwargs):
-
-        model.write('HyperionRT_sed.rtin')
+        self.model.write('HyperionRT_sed.rtin')
         print("Hyperion RT model created.")
-        model.run('HyperionRT_sed.rtout', mpi=True,n_processes=6,overwrite=True)
+
+        self.model.run('HyperionRT_sed.rtout',mpi=useMPI,n_processes=nproc,overwrite=True)
         print("Hyperion RT model executed.")
 
-        result = ModelOutput('HyperionRT_sed.rtout')
-
-        #Save both the model and the results
-        self.model = model
-        self.result = result
+        self.result = ModelOutput('HyperionRT_sed.rtout')
 
     def lnprior(self, theta, **kwargs):
         if self.flatprior:
@@ -169,83 +233,10 @@ class HyperionRTModel(Model):
     def __repr__(self, **kwargs):
         raise NotImplementedError()
 
-    def inputFile(self, **kwargs):
-        #Create a dictionary with which to setup and run Hyperion RT models
-        #Dust parameters
-        par =   {'dust':'"astrosilicate"',
-                 'format':2,
-                 'size':0.5,
-                 'amin':0.5,
-                 'amax':1000.,
-                 'na':101,
-                 'nang':91,
-                 'nanx':11,
-                 'nchem':1,
-                 'gtd':0,
-                 'lmin':0.1,
-                 'lmax':1000.0,
-                 'nl':101,
-                 'massfrac':1.0,
-                 'rho0':1.5e-19,
-                 'optconst':'"silicate_d03.lnk"',
-                 'disttype':'power',
-                 'q':3.5,
-                 #Source parameters
-                 'lsource': 1.0*u.lsun,
-                 'rsource': 1.0*u.rsun,
-                 'msource': 1.0*u.msun,
-                 'tsource': 5784.,
-                 'position':[0.0,0.0,0.0],
-                 #Disc parameters
-                 'rmin': 70.*u.au,
-                 'rmax': 100.*u.au,
-                 'alpha_in': -5.,
-                 'alpha_out': 5.,
-                 'scaleheight': 0.1,
-                 #RT parameters
-                 'niter':5,
-                 'nph_initial':1e4,
-                 'nph_imging':1e5,
-                 'nph_rtsrcs':1e5,
-                 'nph_rtdust':1e5,
-                 #Peel photons to get images
-                 'api_sed':True,
-                 'api_img':False,
-                 }
+    def example(self):
+        density = np.zeros(self.rr)
+        density = rho0 * (self.rr/self.distribution[1])**self.distribution[3] #* np.exp(-((abs(self.model.grid.gz)/self.rr)**2/scaleheight**2)/2.0)
+        density[np.where(self.rr <= self.distribution[1])] = 0.0
+        density[np.where(self.rr >= self.distribution[2])] = 0.0
 
-        return par
-
-    def HyperionRTPlot(self):
-        #
-        # Insert code here to call the plotting routines for the respective data classes
-        # and Hyperion model.
-        #
-
-
-#convenience function to write dust parameter file '<dust>.params' for Hyperion BHDust calculator (separate program)
-    def HyperionRTBHMie(self,par):
-        f=open(par.get('dust','astrosilicate')+'.params','w')
-        f.write('"'+par['dust']+'_'+str(par['size'])+'"'+'\n')
-        f.write(str(par['format'])+'\n')
-        f.write(str(par['amin'])+'\n')
-        f.write(str(par['amax'])+'\n')
-        f.write(str(par['na'])+'\n')
-        f.write(str(par['nang'])+'\n')
-        f.write(str(par['nanx'])+'\n')
-        f.write(str(par['nchem'])+'\n')
-        f.write(str(par['gtd'])+'\n')
-        f.write(str(par['lmin'])+' '+str(par['lmax'])+' '+str(par['nl'])+'\n')
-        f.write(''+'\n')
-        f.write(str(par['massfrac'])+'\n')
-        f.write(str(par['rho0'])+'\n')
-        f.write(str(par['optconst'])+'\n')
-        f.write(str(par['disttype'])+'\n')
-        f.write(str(par['amin'])+' '+str(par['amax'])+' '+str(par['q'])+'\n')
-        f.close()
-
-        print("BHMie dust input file created.")
-
-        import subprocess
-        subprocess.run(['bhmie',param_file])
-
-        print("BHMie dust output file created")
+        return density
