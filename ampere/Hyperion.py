3bb03dc2837b69c5b894e83aae3729a9e8c7a09d
jonty <jonty.marshall@gmail.com>
2021-12-14 17:24:06 +0800

    Added handling for multiple dust components and spatial grids
    The code should now be able to handle dust made out of more than one species with more than one size distribution, and have multiple dust density distributions.

--- ampere/Hyperion.py
+++ ampere/Hyperion.py
@@ -90,22 +90,26 @@ class HyperionRTModel(Model):
         #self.dictionary of fixed parameters that are needed for modelling in __call__
 
     #Only give __call__ the numbers that emcee is going to change.
-    def __call__(self,dust="astrosilicate",fileformat=2,amin=0.5,amax=1000.,na=101,
+    def __call__(self,dust="astrosilicate",fileformat=2,amin=[0.5],amax=[1000.],na=[101],
                         nang=91,nanx=11,
                         nchem=1,gtd=0,
                         lmin=0.1,lmax=1000.0,nl=101,
                         tmin=2.7,tmax=1000.0,nt=101,
-                        massfrac=1.0,
-                        rho0=1.5e-19,
-                        optconst="silicate_d03.lnk",
-                        q=3.5,
+                        massfrac=[1.0],
+                        density=[3.3],
+                        disttype=['power'],
+                        optconst=["silicate_d03.lnk"],
+                        q=[3.5],
                  #Source parameters
                         sources=[['spherical',1.0,1.0,1.0,5784,[0.0,0.0,0.0]]], #(type,lstar,rstar,mstar,tstar,position[x,y,z],spectrum file)
                  #Disc parameters
-                        distribution=['example',30,70,-1], #type,rin,rout,alpha
+                        distribution=[['example',30,70,-1]], #type,rin,rout,alpha
                         gridtype='cartesian',
                         rmax= 100.,
+                        rho0= 1.5e-19,
                         ngrid= 251,
+                #output to be added in SED
+                        components = ['total'],
                         **kwargs):
         
         #Assign keyword variables to the object - there has to be a better way
@@ -126,8 +130,9 @@ class HyperionRTModel(Model):
         self.tmax=tmax
         self.nt=nt
         self.massfrac=massfrac
-        self.rho0=rho0
+        self.density=density
         self.optconst=optconst
+        self.disttype=disttype
         self.q=q
 #Source parameters
         self.sources=sources #(type,lstar,rstar,mstar,tstar,position[x,y,z],spectrum file)
@@ -135,96 +140,91 @@ class HyperionRTModel(Model):
         self.distribution=distribution #type,rin,rout,alpha
         self.gridtype=gridtype
         self.rmax=rmax
+        self.rho0=rho0
         self.ngrid=ngrid
-        try:
-            self.components = len(massfrac) #number of sub-models to run to generate complete model
-        except:
-            self.components = 1
-        #maybe have standard models in here - power law shell = 3 parameters (S0, Rin, alpha_out), 
-        #                                     power law disc = 5 parameters (S0, Rin, Rout, h, alpha_out),
-        #                                     2 power law disc = 5 parameters (S0, R0, h, alpha_in, alpha_out), 
-        #                                     Gaussian annulus = 4 parameters (S0, R0, dR, h)
-        self.npars += len(distribution) - 1
+        self.components = components
+        
+        if nchem != len(massfrac):
+            print("Number of chemical components does not equal mass fractions")
+        
+        if np.sum(massfrac) != 1.0:
+            print("Mass fraction of all chemical components must equal 1")
+        
 
         #Define opacities for use in model calculation - probably only going to be a single species in most cases, but
         #should make it general for multiple species per component, and multiple components (see above)    
         #Read in opacities - do this in __init__, check they exist during __call__
-        import os
-        from scipy import interpolate
-        opacityDirectory = os.path.dirname(__file__)+'/Opacities/'
-        opacityFileList = np.array(opacityFileList)
-        #opacityFileList = np.array(opacityFileList)[['.q' in zio for zio in opacityFileList]] # Only files ending in .q are valid (for now)
-        nSpecies = opacityFileList.__len__()
-        #print(opacityFileList,nSpecies)
-        opacity_array = np.zeros((wavelengths.__len__(), nSpecies))
-        for j in range(0,nchem):
-            tempData = np.loadtxt(opacityDirectory + opacityFileList[j], comments = '#')
-            tempWl = tempData[:, 0]
-            tempOpac = tempData[:, 1]
-            f = interpolate.interp1d(tempWl, tempOpac, assume_sorted = False)
-            opacity_array[:,j] = f(self.wavelength)
-        self.opacity_array = opacity_array
-        self.nSpecies = nchem
-        self.npars += nSpecies - 1 + 4       
 
         #Generating the dust optical constants can be part of __init__, too
         #Convenience function to write dust parameter file '<dust>.params' for Hyperion BHDust calculator (separate program)
+        
         f=open(str(dust)+'.params','w')
         f.write(str(dust)+'_'+str(amin)+'\n')
         f.write(str(int(fileformat))+'\n')
-        f.write(str(amin)+'\n')
-        f.write(str(amax)+'\n')
-        f.write(str(na)+'\n')
+        f.write(str(np.min(amin))+'\n')
+        f.write(str(np.max(amax))+'\n')
+        f.write(str(np.max(na))+'\n')
         f.write(str(nang)+'\n')
         f.write(str(nanx)+'\n')
         f.write(str(nchem)+'\n')
         f.write(str(gtd)+'\n')
         f.write(str(lmin)+' '+str(lmax)+' '+str(nl)+'\n')
-        f.write(str(self.nproc)+'\n') #parallel processing version of BHMie
-        f.write(''+'\n')
-        f.write(str(massfrac)+'\n')
-        f.write(str(rho0)+'\n')
-        f.write(str(optconst)+'\n')
-        f.write(str(self.disttype)+'\n')
-        f.write(str(amin)+' '+str(amax)+' '+str(q)+'\n')
-        f.close()
-
+        f.write(str(nproc)+'\n') #parallel processing version of BHMie
+        for i in range(0,len(self.optconst)):
+            f.write(''+'\n')
+            f.write(str(massfrac[i])+'\n')
+            f.write(str(density[i])+'\n')
+            f.write(str(optconst[i])+'\n')
+            f.write(str(disttype[i])+'\n')
+            f.write(str(amin[i])+' '+str(amax[i])+' '+str(q[i])+'\n')
+            f.close()
+            
+            self.npars += 5 #3 for size distribution, 1 for material, 1 for mass fraction
+            
         print("BHMie dust input file created.")
-
+    
         import subprocess
         subprocess.run(['bhmie',self.param_file])
 
         print("BHMie dust output file created")
-
-        self.d = BHDust(dust+'_'+str(amin))
+        #need a way to iteratively add dust models to the Model object so that they can be called later by name
+        self.d = BHDust(const+'_'+str(amin))
         self.d.optical_properties.extrapolate_nu(5e7, 5e16)
         self.d.set_lte_emissivities(n_temp=self.nt,temp_min=self.tmin,temp_max=self.tmax)
 
-        print("Read in optical constants.")
-        
-        #Add the grid to the model
+        print("Read in optical constants.")            
+               
+        #Add the grid(s) to the model - can specify multiple density grids for different species, physical components
         #Set up spatial grid
         #The grid limits here should be dynamic - set up to fit the largest extent of observations or a 
         #specific field of view or some such
-        if gridtype == 'cartesian':
-            self.x = np.linspace(-1.*rmax*u.au, rmax*u.au, ngrid)
-            self.y = np.linspace(-1.*rmax*u.au, rmax*u.au, ngrid)
-            self.z = np.linspace(-1.*rmax*u.au, rmax*u.au, ngrid)
-            self.model.set_cartesian_grid(self.x,self.y,self.z)
-        #Set up density grid
-            self.rr = np.sqrt(self.model.grid.gx**2 + self.model.grid.gy**2 + self.model.grid.gz**2)
-        if gridtype == 'polar':
-            print("Not implemented.")
-        if gridtype == 'spherical':
-            print("Not implemented")
+        #maybe have standard models in here - power law shell = 3 parameters (S0, Rin, alpha_out), 
+        #                                     power law disc = 5 parameters (S0, Rin, Rout, h, alpha_out),
+        #                                     2 power law disc = 5 parameters (S0, R0, h, alpha_in, alpha_out), 
+        #                                     Gaussian annulus = 4 parameters (S0, R0, dR, h)
+        self.npars += len(self.distribution) - 1
+        
+        for distro in self.distribution:
+        
+            if gridtype == 'cartesian':
+                self.x = np.linspace(-1.*self.rmax*u.au, self.rmax*u.au, ngrid)
+                self.y = np.linspace(-1.*self.rmax*u.au, self.rmax*u.au, ngrid)
+                self.z = np.linspace(-1.*self.rmax*u.au, self.rmax*u.au, ngrid)
+                self.model.set_cartesian_grid(self.x,self.y,self.z)
+            #Set up density grid
+                self.rr = np.sqrt(self.model.grid.gx**2 + self.model.grid.gy**2 + self.model.grid.gz**2)
+            if gridtype == 'polar':
+                print("Not implemented.")
+            if gridtype == 'spherical':
+                print("Not implemented")
 
-        print("Spatial grid set up.")
+            print("Spatial grid set up.")
 
         #define density grid
-        self.density = eval(self.distribution[0] + "()")
-        self.model.add_density_grid(self.density, self.d)
+            self.density = eval(distro[0] + "()")
+            self.model.add_density_grid(self.density, self.d)
 
-        print("Density grid set up.")
+            print("Density grid set up.")
 
         #Set central source position, add source(s) to the grid
         for source in sources: #source with temperature -> blackbody
@@ -261,7 +261,7 @@ class HyperionRTModel(Model):
                     self.source.luminosity = source[1] * u.lsun # [ergs/s]
                     self.source.spectrum = (nu, fnu)
             
-            elif source[0] == 'point'
+            elif source[0] == 'point':
                 if type(source[-1]) == 'list':
                     self.model.add_spherical_source(luminosity  = source[1] * u.lsun,
                                                     radius      = source[2] * u.rsun,
@@ -303,11 +303,11 @@ class HyperionRTModel(Model):
         print("Hyperion RT model executed.")
 
         self.result = ModelOutput('HyperionRT_sed.rtout')
-
-        #Extract fluxes and wavelengths into objects that can be passed to fitting or plotting
-        self.HyperionRTWave = somethingelse
-        self.HyperionRTFlux = something
-
+        #forcing code to return total (scattered + emitted) component here - might want to avoid this if self-scattering is not desired, for example
+        self.HyperionRTSED = self.result.get_sed(inclination=0, aperture=-1, distance=self.dstar * u.pc,component='total',units='mJy')
+        
+        self.wave = self.HyperionRTSED.wav
+        self.flux = self.HyperionRTSED.val
 
     def lnprior(self, theta, **kwargs):
         if self.flatprior:
