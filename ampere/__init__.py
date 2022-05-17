2ef4ab3779d8c8cb98fb3852b22a1dd0fa86239a
Peter Scicluna <peter.scicluna@eso.org>
2022-05-17 09:22:08 -0400

    Updating setup.py

--- ampere/__init__.py
+++ ampere/__init__.py
@@ -3,7 +3,7 @@ Welcome to AMPERE
 
 """
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __copyright__ = """ Copyright (C) 2017  P. Scicluna, F. Kemper, S. Srinivasan
 J.P. Marshall, L. Fanciullo, T. Dharmawardena, A. Trejo, S. Hony
 
