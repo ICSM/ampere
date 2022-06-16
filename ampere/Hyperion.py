b6b1a5d53e3375d9c75a8cb11421cd5c3e2039a3
Sundar Srinivasan <s.srinivasan@irya.unam.mx>
2022-06-15 21:27:28 -0500

    Carbon-star model flux output in Jy instead of mJy

--- ampere/Hyperion.py
+++ ampere/Hyperion.py
@@ -631,7 +631,7 @@ class HyperionCStarRTModel(Model):
 
         self.result = ModelOutput('HyperionRT_sed.rtout')
         #forcing code to return total (scattered + emitted) component here - might want to avoid this if self-scattering is not desired, for example
-        self.HyperionRTSED = self.result.get_sed(inclination=0, aperture=-1, distance=self.dstar * units.pc,component='total',units='mJy')
+        self.HyperionRTSED = self.result.get_sed(inclination=0, aperture=-1, distance=self.dstar * units.pc,component='total',units='Jy')
         
         self.wave = self.HyperionRTSED.wav
         self.flux = self.HyperionRTSED.val
