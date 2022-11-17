7f1d5dbcaa1b9fd1254b01046a49211300f2d270
jonty <jmarshall@asiaa.sinica.edu.tw>
2022-11-17 18:16:21 +0800

    Added star + disc example to ampere
    Basic version of the star + disc fitting (treating both as blackbodies) now runs.

--- ampere/QuickSED.py
+++ ampere/QuickSED.py
@@ -1,10 +1,11 @@
 #
 # Ampere Stuff
 #
-
+import ampere
+from ampere.models import Model
 import numpy as np
 from astropy import constants as const
-from astropy import units
+from astropy import units as u
 try:
     from astropy.modeling import blackbody
 except ImportError:
@@ -12,12 +13,7 @@ except ImportError:
 from .models import AnalyticalModel
 
 from scipy.stats import dirichlet
-
-# 
-# QuickSED Stuff
-# 
-
-import QuickSED
+from scipy.stats import uniform
 
 #
 # Other Stuff
@@ -25,69 +21,98 @@ import QuickSED
 
 import subprocess
 
-# Silly string to link Ampere and Hyperion together
+#constants
+h = 6.626e-34
+c = 299792458.0 # m/s
+k = 1.38e-23
+sb = 5.67e-8 #
+au     = 1.495978707e11 # m 
+pc     = 3.0857e16 # m
+lsol   = 3.828e26 # W
+rsol   = 6.96342e8 # m
+MEarth = 5.97237e24 # kg
+
+um = 1e-6 #for wavelengths in microns
+
 class QuickSEDModel(Model):
-    '''
-    Class to link Ampere and Hyperion.
+    '''This is a very simple model in which the flux is linear in wavelength.
+
+    This model shows you the basics of writing a model for ampere
 
     '''
+    def __init__(self, wavelengths, flatprior = True,
+                 lims = np.array([[0.,1e30], #pc
+                                  [1.0,1e5], #K
+                                  [0.0,1e3], #Rsun
+                                  [0.0,1.0], #Ldust/Lstar
+                                  [1.0,1e5], #K
+                                  [0.0,1e3], #micron 
+                                  [-3.0,3.0]]),**kwargs):
 
-    def __init__(self,directory = '',
-                        prefix = 'new_qsed_model',
-                        obsv = False,
-        #wavelength grid
-                        wave_min = 3e-1,
-                        wave_max = 3e3,
-                        nwave = int(1001),
-        #stellar model
-                        dstar = 10.0,
-                        tstar = 5770.0,
-                        rstar = 1.0,
-        #disc model
-                        fdisc = [1e-4],
-                        tdisc = [300.0],
-                        lam0  = [200.0],
-                        beta  = [1.5],
-        #outputs
-                        sed_star  = [],
-                        sed_disc  = [],
-                        sed_total = [],
-                        **kwargs):
-        
-        #Assign keyword variables to the object
-        #Assign all the inputs to __init__ to instance variables with the same name as above
-        #this is equivalent to many lines of self.niter = niter etc
+        '''The model constructor, which will set everything up
+
+        This method does essential setup actions, primarily things that 
+        may change from one fit to another, but will stay constant throughout 
+        the fit. This may be things like the grid of wavelengths to calculate
+        the model output on, or establishing the dust opacities if involved.
+        There are also several important variables it *MUST* define here
+        '''
+        self.wave = wavelengths
+        self.freq = (wavelengths * u.micron).to(u.Hz, equivalencies=u.spectral()).value
+        self.npars = 7 #Number of free parameters for the model (__call__()). For some models this can be determined through introspection, but it is still strongly recommended to define this explicitly here. Introspection will only be attempted if self.npars is not defined.
+        self.npars_ptform = 7 #Sometimes the number of free parameters is different when using the prior transform instead of the prior. In that case, self.npars_ptform should also be defined.
+        #You can do any other set up you need in this method.
+        #For example, we could define some cases to set up different priors
+        #But that's for a slightly more complex example.
+        #Here we'll just use a simple flat prior
+        self.lims = lims
+        self.flatprior = flatprior
+        self.parLabels = ['dstar','tstar','rstar','fdust','tdust','lam0','beta']
+
+        '''Assign any other keywords.'''
         l = locals()
         for key, value in l.items():
             if key == "self": #skip self to avoid possible recursion
                 continue
             setattr(self, key, value)
 
-        self.model = QuickSED.QSED(self)
-        
-        print("QuickSED Model setup complete.")
-        #self.dictionary of fixed parameters that are needed for modelling in __call__
+    def __call__(self,dstar,tstar,rstar,fdust,tdust,lam0,beta,**kwargs):
+        '''The model itself, using the callable class functionality of python.
 
-    #Only give __call__ the numbers that emcee is going to change.
-    def __call__(self,**kwargs):
+        This is an essential method. It should do any steps required to 
+        calculate the output fluxes. Once done, it should stop the output fluxes
+        in self.modelFlux.
+        '''
 
-        #Same as in __init__!
         l = locals()
         for key, value in l.items():
             if key == "self": #skip self to avoid possible recursion
                 continue
             setattr(self, key, value)
+
+        self.lstar = 4.*np.pi*(self.rstar*rsol)**2*sb*self.tstar**4 / lsol
+
+        sc = 1e26*(1./(self.dstar*pc)**2)*(self.wave*um)**2 /c
+
+        photosphere = (rstar*rsol)**2*np.pi*BlackBody().evaluate(self.freq, self.tstar, 1*u.Jy/u.sr)
+
+        photopshere = photosphere * ( self.lstar / np.trapz(photosphere) ) * sc
+
+        self.star = photosphere
+
+        modified = np.where(self.wave >= self.lam0) 
         
-        self.npars += len(self.lam0) + len(self.beta) + len(self.tdisc) + 3 - 1 #factor 3 comes from stellar parameters
+        emission = BlackBody().evaluate(self.freq, self.tdust, 1*u.Jy/u.sr)    
 
-        self.model = QuickSED.QSED(self)
+        emission[modified] = emission[modified]*(self.lam0/self.wave[modified])**beta
 
-        self.model.wave()
-        self.model.star()
-        self.model.disc()
+        emission = emission*(self.fdust*np.trapz(self.star)/np.trapz(emission))*sc
+        
+        self.dust = emission
 
-        self.wave = self.model.sed_wave
-        self.flux = self.model.sed_total
+        self.modelFlux = self.star + self.dust
+
+        return {"spectrum":{"wavelength":self.wave, "flux": self.modelFlux}}
 
     def lnprior(self, theta, **kwargs):
         if self.flatprior:
@@ -95,10 +120,26 @@ class QuickSEDModel(Model):
                (self.lims[1,0] < theta[1] < self.lims[1,1]) and \
                (self.lims[2,0] < theta[2] < self.lims[2,1]) and \
                (self.lims[3,0] < theta[3] < self.lims[3,1]) and \
-                np.sum(10**theta[4:]) <= 1. and np.all(theta[4:] < 0.): 
+               (self.lims[4,0] < theta[4] < self.lims[4,1]) and \
+               (self.lims[5,0] < theta[5] < self.lims[5,1]) and \
+               (self.lims[6,0] < theta[6] < self.lims[6,1]): 
                 return 0
             else:
                 return -np.inf
+        else:
+            raise NotImplementedError()
+
+    def prior_transform(self, u, **kwargs):
+        '''The prior transform, which takes samples from the Uniform(0,1) distribution to the desired distribution.
+
+        This is only included for completeness and to demonstrate how a prior 
+        transform function should look. This example only uses emcee for 
+        fitting, which uses the lnprior function instead. Prior transforms are 
+        required by nested-sampling codes and similar approaches.
+        '''
+        if self.flatprior:
+            theta = np.zeros_like(u)
+            return (self.lims[:,1] - self.lims[:,0]) * u + self.lims[:,0]
         else:
             raise NotImplementedError()
 
@@ -108,3 +149,9 @@ class QuickSEDModel(Model):
     def __repr__(self, **kwargs):
         from pprint import pformat
         return "<" + type(self).__name__ + "> " + pformat(vars(self), indent=4, width=1)
+
+
+if __name__ == "__main__":
+    """ Set up the inputs for the model """
+    """ wavelength grid """
+    raise NotImplementedError()
---
+++ examples/star_disc/model_test_rt.py
@@ -0,0 +1,43 @@
+import ampere
+from ampere import data
+from ampere.infer.emceesearch import EmceeSearch
+from ampere.infer.sbi import SBI_SNPE
+import ampere.QuickSED
+import numpy as np
+import dill
+
+libdir= ampere.__file__.strip('__init__.py')
+
+p = data.Photometry.fromFile('HD105_SED.vot', format = 'votable',libName = libdir+ 'ampere_allfilters.hd5')
+
+p.reloadFilters(np.logspace(np.log10(0.2),np.log10(500),1000,base=10))
+
+s = data.Spectrum.fromFile('HD105_IRS_Spectrum.csv', 'User-Defined', filetype = 'text', keywords = {'waveCol': 'wavelength', 'fluxCol': 'fluxdensity', 'uncsCol': 'uncertainty', 'waveUnit': 'micron', 'fluxUnit': 'Jy'})
+
+dataset = [p]
+#for sp in s:
+#	dataset.append(sp)
+
+wavelengths = np.logspace(np.log10(0.1),np.log10(500),1000,endpoint=True)
+
+model = ampere.QuickSED.QuickSEDModel(wavelengths,dstar=40.0,tstar=5770.0,rstar=1.0,fdisc=1e-4,\
+                        tdisc=50.0,lam0=200.0,beta=1.5)
+
+print(model.__repr__)
+
+optimizer = EmceeSearch(model=model,data=dataset,nwalkers=20,vectorize=False)
+
+#self.parLabels = ['dstar','tstar','rstar','fdust','tdust','lam0','beta'] + noise parameters
+guess = [20.0,6000.0,1.0,1e-4,100.0,200.0,1.0]#,1.0,0.5,1.0,1.0,0.5,1.0,1.0,0.5,1.0,1.0,0.5,1.0,1.0,0.5,1.0,1.0,0.5,1.0]
+
+optimizer.optimise(nsamples=200,nburnin=25,guess=guess)
+
+optimizer.summary()
+
+optimizer.print_summary(outfile='jonty_quicksed_test_summary.txt')
+
+optimizer.postProcess()
+
+#save object before crashing.
+with open('jonty_quicksed_test.pkl', 'wb') as file:
+	dill.dump(optimiser.samples,file)
