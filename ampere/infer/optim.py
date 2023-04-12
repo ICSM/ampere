2c733af60d514b35314106e5c70369ab462e514b
Peter Scicluna <pscicluna@users.noreply.github.com>
2023-04-12 20:37:07 +0100

    Getting a reasonable __init__ up and running for the base class

--- ampere/infer/optim.py
+++ ampere/infer/optim.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 
 import numpy as np
 import pickle
+from datetime import datetime
 from ..logger import Logger
 from .basesearch import BaseSearch
 from inspect import signature
@@ -36,7 +37,101 @@ class OptimBase(BaseSearch, Logger):
         Options: 'full', 'short', 'stamp', 'model'.
     """
 
-    pass
+    def __init__(self, 
+                 model=None,
+                 data=None,
+                 verbose=False,
+                 parameter_labels=None,
+                 name='',
+                 namestyle="full",
+                 **kwargs):
+        self.model = model
+        self.dataSet = data
+        self.namestyle = namestyle
+        self.name = name
+        self.verbose = verbose
+
+        self.setup_logging(verbose=verbose)
+        self.logger.info("Welcome to ampere")
+        self.logger.info("Setting up your inference problem:")
+        self.logger.info("You are using %s", self._inference_method)
+        self.logger.info("You have %s items in your dataset", str(len(data)))
+
+        try:
+            self.nparsMod = self.model.npars
+        except AttributeError:
+            sig = signature(model.__call__)
+            # Always subtract **kwargs from the parameters, but don't need to
+            # worry about self once it is bound to an instance
+            self.nparsMod = len(sig.parameters) - 1
+        # number of parameters to be passed into each set of data
+        self.nparsData = [data.npars for data in self.dataSet]
+        self.npars = int(self.nparsMod + np.sum(self.nparsData))
+
+        if parameter_labels is None:
+            # The user hasn't specified parameter labels, let's see if the
+            # models and data have instead
+            try:  # First the model parameters
+                self.parLabels = self.model.parLabels
+            except AttributeError:
+                self.parLabels = [f'x{str(i)}' for i in range(self.nparsMod)]
+            i = self.nparsMod
+            for data in self.dataSet:
+                try:
+                    self.parLabels.extend(data.parLabels)
+                except AttributeError:
+                    self.parLabels.extend([f'x{str(i)}'
+                                           for i in range(i, i+data.npars)])
+                finally:
+                    i += data.npars
+        else:  # User isn't really supposed to use this interface, however...
+            self.parLabels = parameter_labels
+
+        self.verbose = verbose
+        # if self.verbose:
+        self.logger.info("This model has %d parameters.", self.nparsMod)
+        self.logger.info("There are also %d parameters for the noise model",
+                         self.npars - self.nparsMod)
+        self.logger.info("Hence, there are a total of %d parameters to sample",
+                         self.npars)
+        self.logger.info("The parameter names are:")
+        for label in self.parLabels:
+            self.logger.info("%s", label)
+
+        # Now we should check whether the number of parameters matches with
+        # the parameter labels!
+        if len(self.parLabels) != self.npars:
+            self.logger.critical("You have %d free parameters but %d parameter labels",
+                                 self.npars, len(self.parLabels))
+            self.logger.critical("Please check the number of parameters and labels")
+            raise ValueError("Mismatch between number of parameters and labels")
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name=''):
+        try:
+            modelname = self.model.name
+        except AttributeError:
+            modelname = self.model.__class__.__name__
+        if self.namestyle == 'full':
+            self._name = ("ampere_"+str(datetime.now()
+                                       ).replace(' ',
+                                                 '_').replace(":", "-")[:-7]
+                          + "_" + modelname + "_" + name
+                          )
+        elif self.namestyle == "model":
+            self._name = f"ampere_{modelname}_{name}"
+        elif self.namestyle == "short":
+            self._name = name
+        elif self.namestyle == "stamp":
+            self.name = ("ampere_"+str(datetime.now()).replace(' ',
+                                                               '_').replace(":",
+                                                                            "-")[:-7]
+                                  + "_" + name
+                         )
 
 
 class ScipyMinOpt(OptimBase):
