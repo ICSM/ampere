615442cbc0f345f8231973017fe0cef190697466
Peter Scicluna <pscicluna@users.noreply.github.com>
2023-04-12 01:59:27 +0100

    More lint-related updates
    Cleaning up some of the linting errors (long lines, too much or too little whitespace, other PEP8 violations) throughout some of the MCMC interfaces. This both leaves me fewer errors to deal with and mostly makes the code easier to read. Some lines are difficult to shorten, however.

--- ampere/infer/basesearch.py
+++ ampere/infer/basesearch.py
@@ -5,30 +5,31 @@ import pickle
 from ..logger import Logger
 from ..models.results import ModelResults
 
+
 class BaseSearch(object):
     """ A base class for parameter search algorithms.
 
     This is the base class from which all other search classes will be derived.
-    It doesn't implement much, other than the prior, posterior and likelihood 
+    It doesn't implement much, other than the prior, posterior and likelihood
     functions. It also implements a general method for serialising (pickling)
     the search objects, so that they can be saved for later or to make parallel
-    execution easier. __getstate__ and __setstate__ should be implemented as I 
-    expect non of ampere's classes will be easily pickle/dill-able. The methods 
-    defined here only need to be re-implemented if you expect to change their 
-    behaviour significantly, and the NotImplemented methods must be defined in 
+    execution easier. __getstate__ and __setstate__ should be implemented as I
+    expect non of ampere's classes will be easily pickle/dill-able. The methods
+    defined here only need to be re-implemented if you expect to change their
+    behaviour significantly, and the NotImplemented methods must be defined in
     subclasses to make sure everything is useful.
-    
     """
-    
+
     def __init__(self, model=None, data=None, **kwargs):
         '''
-        Initialise the optimiser with a model and some data, then use introspection on both to establish a few important numbers 
+        Initialise the optimiser with a model and some data, then use
+        introspection on both to establish a few important numbers
         '''
         raise NotImplementedError()
 
     def __call__(self, **kwargs):
         raise NotImplementedError()
-    
+
     def __str__(self, **kwargs):
         raise NotImplementedError()
 
@@ -39,90 +40,96 @@ class BaseSearch(object):
         """
         We delegate the prior transforms to the models and the data
         """
-        #print(u)
+        # print(u)
         theta = np.zeros_like(u)
         theta[:self.nparsMod] = self.model.prior_transform(u[:self.nparsMod])
         i = self.nparsMod
         for data in self.dataSet:
             theta[i:i+data.npars] = data.prior_transform(u[i:i+data.npars])
-            i+=data.npars
+            i += data.npars
         return theta
-    
+
     def lnprior(self, theta, **kwargs):
         """ Calculate the probability of the model (the prior)
-        
+
         We delegate the priors to the models and the data
         """
         lp = self.model.lnprior(theta[:self.nparsMod])
         i = self.nparsMod
         for data in self.dataSet:
-            lp+= data.lnprior(theta[i:i+data.npars])
-            i+=data.npars
+            lp += data.lnprior(theta[i:i+data.npars])
+            i += data.npars
         return lp
 
     def lnlike(self, theta, **kwargs):
         """ Calculate the likelihood of the model given the data
 
         This routine takes a simple approach to calculating the log-likelihood
-        of the model given the data. It calculates the model, then loops over 
+        of the model given the data. It calculates the model, then loops over
         all items in the dataset and calculates their likelihoods.
 
         """
         result = self.model(*theta[:self.nparsMod])
 
         if not isinstance(result, ModelResults):
-            result = ModelResults(**result) #Try unpacking the results here if the user didn't already define their model with it
-        l=np.array([])
+            # Try unpacking the results here if the user didn't already define
+            # their model with it
+            result = ModelResults(**result)
+        l = np.array([])
         i = self.nparsMod
         for data in self.dataSet:
-            lnew = data.lnlike(theta[i:i+data.npars],result)
-            i+=data.npars
-            l = np.r_[l,lnew]
+            lnew = data.lnlike(theta[i:i+data.npars], result)
+            i += data.npars
+            l = np.r_[l, lnew]
         return np.sum(l)
-            
-        #raise NotImplementedError()
 
-    def lnprob(self, theta):#, dataSet, **kwargs):
+        # raise NotImplementedError()
+
+    def lnprob(self, theta):  # , dataSet, **kwargs):
         """ Calculate the probability of the model given the data
-        
-        This just calls the prior, followed by the likelihood, and returns the 
+
+        This just calls the prior, followed by the likelihood, and returns the
         sum of the two.
         """
-        
-        p = self.lnprior(theta)#[:self.nparsMod])
+
+        p = self.lnprior(theta)  # [:self.nparsMod])
         if p == -np.inf:
             return p
         return p + self.lnlike(theta)
 
     def lnprob_vector(self, thetas):
-        ''' This method is a drop-in replacement for the above method but where theta is a vector of arguments. 
+        ''' This method is a drop-in replacement for the above method but
+        where theta is a vector of arguments.
 
-        It assumes no parallelisation, and that the vectorisation is just being used to cut down on function calls
+        It assumes no parallelisation, and that the vectorisation is just
+        being used to cut down on function calls
         '''
 
-        #print(theta)
-        ##lnprobs = pool.map(lnprob_pool, theta, worker_init=self.init_workers)
+        # print(theta)
+        # lnprobs = pool.map(lnprob_pool, theta, worker_init=self.init_workers)
         if len(thetas.shape) > 2:
-            raise TypeError("Input parameters have too many indices: expected 2, got {}".format(len(theta.shape)))
+            raise TypeError(
+                "Input parameters have too many indices: expected 2, "
+                f"got {len(thetas.shape)}"
+            )
         lps = self.lnprior_vector(thetas)
         mask = np.isfinite(lps)
         lps[mask] += self.lnlike_vector(thetas[mask])
-        #lls = self.lnlike_vector(thetas)
-        return lps#+lls
-        #n_samples = thetas.shape[0]
-        #lps = np.zeros(n_samples)
-        #lps = self.lnprior_vector(thetas)
-        #for i, theta in enumerate(thetas):
+        # lls = self.lnlike_vector(thetas)
+        return lps  # +lls
+        # n_samples = thetas.shape[0]
+        # lps = np.zeros(n_samples)
+        # lps = self.lnprior_vector(thetas)
+        # for i, theta in enumerate(thetas):
         #    lps[i] = self.lnprob(theta)
-        #return lps
+        # return lps
 
-        #n_samples = thetas.shape[0]
-        #lps = np.zeros(n_samples)
-        #lps = self.lnprior_vector(thetas)
-        #for i, theta in enumerate(thetas):
+        # n_samples = thetas.shape[0]
+        # lps = np.zeros(n_samples)
+        # lps = self.lnprior_vector(thetas)
+        # for i, theta in enumerate(thetas):
         #    lps[i] += self.lnlike(theta)
-        #return lps
-        
+        # return lps
 
     def lnprior_vector(self, thetas):
         n_samples = thetas.shape[0]
@@ -131,52 +138,55 @@ class BaseSearch(object):
             lp = self.model.lnprior(theta[:self.nparsMod])
             i = self.nparsMod
             for data in self.dataSet:
-                lp+= data.lnprior(theta[i:i+data.npars])
-                i+=data.npars
+                lp += data.lnprior(theta[i:i+data.npars])
+                i += data.npars
             lps[j] = lp
         return lps
 
     def lnlike_vector(self, thetas):
         n_samples = thetas.shape[0]
-        #print(thetas.shape)
+        # print(thetas.shape)
         lps = np.zeros(n_samples)
         for j, theta in enumerate(thetas):
-            #print(theta)
+            # print(theta)
             result = self.model(*theta[:self.nparsMod])
             if not isinstance(result, ModelResults):
-                result = ModelResults(**result) #Try unpacking the results here if the user didn't already define their model with it
-            l=np.array([])
+                # Try unpacking the results here if the user didn't already
+                # define their model with it
+                result = ModelResults(**result)
+            l = np.array([])
             i = self.nparsMod
             for data in self.dataSet:
-                lnew = data.lnlike(theta[i:i+data.npars],result)
-                i+=data.npars
-                l = np.r_[l,lnew]
+                lnew = data.lnlike(theta[i:i+data.npars], result)
+                i += data.npars
+                l = np.r_[l, lnew]
             lps[j] = np.sum(l)
         return lps
-            
 
     def lnprob_vector_pool(self, theta, pool):
-        ''' This method is a drop-in replacement for the above method but where theta is a vector of arguments. 
+        ''' This method is a drop-in replacement for the above method but
+        where theta is a vector of arguments.
 
         It assumes you want to parallelise execution of models across a pool.
 
         This method is not yet implemented.
         '''
         pass
-        
+
     def lnprob_vector_dask(self, theta, **kwargs):
         pass
 
     def mpire_setup(self):
-        ''' Do some setup steps to initialise the WorkerPool and have it ready for parallel processing. 
+        ''' Do some setup steps to initialise the WorkerPool and have it ready
+        for parallel processing.
         '''
         from mpire import WorkerPool
-        
-        pool = WorkerPool(njobs = self.njobs,
+
+        pool = WorkerPool(njobs=self.njobs,
                           use_worker_state=True,
                           keep_alive=True,
                           use_dill=self.use_dill
-        )
+                          )
 
         self.init_workers = create_worker_init(self)
         return pool
@@ -184,20 +194,22 @@ class BaseSearch(object):
     def sampler(self, **kwargs):
         raise NotImplementedError()
 
-    def save(self, filename, pickle_it=True,**kwargs):
-        ''' A method to save the object to a file. For the moment, this only supports pickling '''
+    def save(self, filename, pickle_it=True, **kwargs):
+        ''' A method to save the object to a file. For the moment, this only
+        supports pickling '''
         if not pickle_it:
-            raise NotImplementedError('Only pickling is supported at this time')
+            raise NotImplementedError('Only pickling is supported at this time'
+                                      )
         with open(filename, 'wb') as f:
             pickle.dump(self.__dict__, f)
 
-
-    #def __getstate__(self):
+    # def __getstate__(self):
     #    pass
 
-    #def __setstate__(self)
+    # def __setstate__(self)
 
-def create_worker_init(searcher):    
+
+def create_worker_init(searcher):
     def init_workers(worker_state):
         worker_state['searcher'] = searcher
     return init_workers
@@ -206,8 +218,6 @@ def create_worker_init(searcher):
 def lnlike_pool(worker_state, theta):
     return worker_state['searcher'].lnlike(theta)
 
+
 def lnprob_pool(worker_state, theta):
     return worker_state['searcher'].lnprob(theta)
-
-
-    
--- ampere/infer/mcmcsearch.py
+++ ampere/infer/mcmcsearch.py
@@ -1,11 +1,11 @@
 from __future__ import print_function
 
-import logging
 import numpy as np
 from inspect import signature
 from datetime import datetime
 from .basesearch import BaseSearch
 from ..logger import Logger
+
 
 class MCMCSampler(BaseSearch, Logger):
 
@@ -17,26 +17,24 @@ class MCMCSampler(BaseSearch, Logger):
     Parameters
     ----------
     model : ampere.model.Model
-    Model which will be used
+        Model whose posterior will be inferred
     data : list or iterable of ampere.data.Data
-    the dataset to fit
+        the dataset to fit
     verbose : bool, optional
-    If True, print verbose output.
+        If True, print verbose output.
     parameter_labels : list of str, optional
-    List of strings containing names of each parameter.
+        List of strings containing names of each parameter.
     name : str, optional
-    Name of the sampler.
+        Name of the sampler.
     namestyle : str, optional
-    String specifying style for naming the sampler.
-    Options: 'full', 'short', 'stamp', 'model'.
-    
-    Generated with Chat-GPT
+        String specifying style for naming the sampler.
+        Options: 'full', 'short', 'stamp', 'model'.
     """
 
     _inference_method = "MCMC"
 
-    def __init__(self, model = None, data= None, verbose = False,
-                 parameter_labels = None, name='', namestyle="full",
+    def __init__(self, model=None, data=None, verbose=False,
+                 parameter_labels=None, name='', namestyle="full",
                  **kwargs):
         self.model = model
         self.dataSet = data
@@ -45,69 +43,81 @@ class MCMCSampler(BaseSearch, Logger):
             modelname = model.name
         except AttributeError:
             modelname = model.__class__.__name__
-        if namestyle=='full':
-            self.name = "ampere_"+str(datetime.now()).replace(' ','_').replace(":","-")[:-7] + "_" + modelname + "_" + name
-        elif namestyle=="short":
+        if namestyle == 'full':
+            self.name = ("ampere_"+str(datetime.now()
+                                       ).replace(' ',
+                                                 '_').replace(":", "-")[:-7]
+                         + "_" + modelname + "_" + name
+                         )
+        elif namestyle == "short":
             self.name = name
-        elif namestyle=="stamp":
-            self.name = "ampere_"+str(datetime.now()).replace(' ','_').replace(":","-")[:-7] + "_" + name
-        elif namestyle=="model":
-            self.name = "ampere_"+modelname + "_" + name
-        
-        
+        elif namestyle == "stamp":
+            self.name = ("ampere_"+str(datetime.now()).replace(' ',
+                                                               '_').replace(":",
+                                                                            "-")[:-7]
+                                  + "_" + name
+                         )
+        elif namestyle == "model":
+            self.name = f"ampere_{modelname}_{name}"
 
-        self.setup_logging(verbose=verbose) #For now we will exclusively use default logging settings, this will be modified once logging is tested.
+        # For now we will exclusively use default logging settings, this will
+        #  be modified once logging is tested.
+        self.setup_logging(verbose=verbose)
         self.logger.info("Welcome to ampere")
         self.logger.info("Setting up your inference problem:")
         self.logger.info("You are using %s", self._inference_method)
         self.logger.info("You have %s items in your dataset", str(len(data)))
 
-        ''' now do some introspection on the various bits of model to 
-        understand how many parameters there are for each compponent '''
+        # now do some introspection on the various bits of model to
+        # understand how many parameters there are for each compponent
         try:
             self.nparsMod = self.model.npars
-        except:
+        except AttributeError:
             sig = signature(model.__call__)
-            self.nparsMod = len(sig.parameters) - 1 #Always subtract **kwargs from the parameters, but don't need to worry about self once it is bound to an instance
-        self.nparsData = [data.npars for data in self.dataSet] #number of parameters to be passed into each set of data
+            # Always subtract **kwargs from the parameters, but don't need to
+            # worry about self once it is bound to an instance
+            self.nparsMod = len(sig.parameters) - 1
+        # number of parameters to be passed into each set of data
+        self.nparsData = [data.npars for data in self.dataSet]
         self.npars = int(self.nparsMod + np.sum(self.nparsData))
 
         if parameter_labels is None:
-            #The user hasn't specified parameter labels, let's see if the models and data have instead
-            try: #First the model parameters
+            # The user hasn't specified parameter labels, let's see if the
+            # models and data have instead
+            try:  # First the model parameters
                 self.parLabels = self.model.parLabels
             except AttributeError:
-                self.parLabels = ['x'+str(i) for i in range(self.nparsMod)]
+                self.parLabels = [f'x{str(i)}' for i in range(self.nparsMod)]
             i = self.nparsMod
             for data in self.dataSet:
                 try:
                     self.parLabels.extend(data.parLabels)
                 except AttributeError:
-                    self.parLabels.extend(['x'+str(i) for i in range(i, i+data.npars)])
+                    self.parLabels.extend([f'x{str(i)}'
+                                           for i in range(i, i+data.npars)])
                 finally:
-                    i+=data.npars
-        else: #User isn't really supposed to use this interface, however...
+                    i += data.npars
+        else:  # User isn't really supposed to use this interface, however...
             self.parLabels = parameter_labels
-        #self.parLabels = ['x'+str(i) for i in range(self.npars)] #Parameter for parameter names (labels) to associate with output in post processing - emcee does this internally with parameter_names, but we want a universal system across all the search methods. Called parLabels to distinguish it from Data Class labels.
-
-
-        
 
         self.verbose = verbose
-        #if self.verbose:
+        # if self.verbose:
         self.logger.info("This model has %d parameters.", self.nparsMod)
-        self.logger.info("There are also %d parameters for the noise model", self.npars - self.nparsMod)
-        self.logger.info("Hence, there are a total of %d parameters to sample", self.npars)
+        self.logger.info("There are also %d parameters for the noise model",
+                         self.npars - self.nparsMod)
+        self.logger.info("Hence, there are a total of %d parameters to sample",
+                         self.npars)
         self.logger.info("The parameter names are:")
-        for l in self.parLabels:
-            self.logger.info("%s", l)
+        for label in self.parLabels:
+            self.logger.info("%s", label)
 
-        #Now we should check whether the number of parameters matches with the parameter labels!
+        # Now we should check whether the number of parameters matches with
+        # the parameter labels!
         if len(self.parLabels) != self.npars:
-            self.logger.critical("You have %d free parameters but %d parameter labels", self.npars, len(self.parLabels))
+            self.logger.critical("You have %d free parameters but %d parameter labels",
+                                 self.npars, len(self.parLabels))
             self.logger.critical("Please check the number of parameters and labels")
             raise ValueError("Mismatch between number of parameters and labels")
-            
 
 
 class EnsembleSampler(MCMCSampler):
@@ -118,88 +128,88 @@ class EnsembleSampler(MCMCSampler):
     Parameters
     ----------
     model : ampere.model.Model
-    Model which will be used
+        Model which will be used
     data : list or iterable of ampere.data.Data
-    the dataset to fit
+        the dataset to fit
     nwalkers : int
-    number of walkers to use in the ensemble
+        number of walkers to use in the ensemble
     verbose : bool, optional
-    If True, print verbose output.
+        If True, log verbosely.
     parameter_labels : list of str, optional
-    List of strings containing names of each parameter.
+        List of strings containing names of each parameter.
     name : str, optional
-    Name of the sampler.
+        Name of the sampler.
     namestyle : str, optional
-    String specifying style for naming the sampler.
-    Options: 'full', 'short', 'stamp', 'model'.
-    
-    Generated with Chat-GPT
-
+        String specifying style for naming the sampler.
+        Options: 'full', 'short', 'stamp', 'model'.
     """
 
     _inference_method = "Ensemble MCMC"
 
-    def __init__(self, model = None, data= None, verbose = False,
-                 parameter_labels = None, name='', namestyle="full",
-                 nwalkers = None, 
+    def __init__(self, model=None, data=None, verbose=False,
+                 parameter_labels=None, name='', namestyle="full",
+                 nwalkers=None,
                  **kwargs):
 
-        super().__init__(model = model, data= data, verbose = verbose,
-                         parameter_labels = parameter_labels, name = name,
+        super().__init__(model=model, data=data, verbose=verbose,
+                         parameter_labels=parameter_labels, name=name,
                          namestyle=namestyle, **kwargs)
 
         self.nwalkers = nwalkers
         self.logger.info("This problem will be sampled with %d walkers", self.nwalkers)
 
-    def rebuildSampler(self, nwalkers = None, model = None,
-                       data = None, lnprior = None,
-                       labels = None, 
+    def rebuildSampler(self, nwalkers=None, model=None,
+                       data=None, lnprior=None,
+                       labels=None,
                        **kwargs):
 
         """
         Rebuilds the MCMC sampler with the specified parameters.
-        
+
         Parameters
         ----------
         nwalkers : int, optional
-        The number of walkers to use in the MCMC sampling. If not provided, the current number of walkers will be used.
+            The number of walkers to use in the MCMC sampling. If not provided,
+            the current number of walkers will be used.
         model : object, optional
-        A new model to use in the MCMC sampling. If not provided, the current model will be used.
+            A new model to use in the MCMC sampling. If not provided, the
+            current model will be used.
         data : object, optional
-        New data to use in the MCMC sampling. If not provided, the current data will be used.
+            New data to use in the MCMC sampling. If not provided, the current
+            data will be used.
         lnprior : function, optional
-        A new prior distribution to use in the MCMC sampling. If not provided, the current prior will be used.
+            A new prior distribution to use in the MCMC sampling. If not
+            provided, the current prior will be used.
         labels : list of str, optional
-        A list of parameter labels to use in post-processing. If not provided, the
-        
-        current labels will be used.
-        
+            A list of parameter labels to use in post-processing. If not
+            provided, the current labels will be used.
         **kwargs : additional keyword arguments
-Additional keyword arguments to pass to the sampler.
-
-        Generated with Chat-GPT
+            Additional keyword arguments to pass to the sampler.
         """
 
         self.logger.info("Rebuilding your sampler")
         if np.any([model, data]):
             if model is not None:
                 self.logger.info("Your model is being updated")
-                self.model=model
+                self.model = model
                 try:
                     self.nparsMod = self.model.npars
-                except:
+                except AttributeError:
                     sig = signature(model.__call__)
-                    self.nparsMod = len(sig.parameters) - 1 #Always subtract **kwargs from the parameters, but don't need to worry about self once it is bound to an instance
+                    # Always subtract **kwargs from the parameters, but don't
+                    # need to worry about self once it is bound to an instance
+                    self.nparsMod = len(sig.parameters) - 1
             if data is not None:
                 self.dataSet = data
                 self.logger.info("Your data is being updated")
-                self.nparsData = [data.npars for data in self.dataSet] #number of parameters to be passed into each set of data
+                # number of parameters to be passed into each set of data
+                self.nparsData = [data.npars for data in self.dataSet]
 
             self.npars = int(self.nparsMod + np.sum(self.nparsData))
-        
+
         if lnprior is not None:
             self.lnprior = lnprior
-        #print(self.npars, self.nparsMod, self.nparsData)
+        # print(self.npars, self.nparsMod, self.nparsData)
         if nwalkers is not None:
-                self.nwalkers=nwalkers
-                self.logger.info("The number of walkers is now %d", self.nwalkers)
+            self.nwalkers = nwalkers
+            self.logger.info("The number of walkers is now %d", self.nwalkers)
